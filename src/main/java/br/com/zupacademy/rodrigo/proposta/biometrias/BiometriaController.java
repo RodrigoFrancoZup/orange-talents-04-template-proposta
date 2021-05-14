705cbf075bbc464a1d5c49b41e188e6f155a5e9f
Rodrigo Franco <rodrigo.franco@zup.com.br>
2021-05-14 08:23:13 -0300

    Validando o fingerprint em Base64

--- src/main/java/br/com/zupacademy/rodrigo/proposta/biometrias/BiometriaController.java
+++ src/main/java/br/com/zupacademy/rodrigo/proposta/biometrias/BiometriaController.java
@@ -2,6 +2,7 @@ package br.com.zupacademy.rodrigo.proposta.biometrias;
 
 import br.com.zupacademy.rodrigo.proposta.cartoes.Cartao;
 import br.com.zupacademy.rodrigo.proposta.cartoes.CartaoRepository;
+import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -27,10 +28,14 @@ public class BiometriaController {
     public ResponseEntity<?> cadastra(@PathVariable String cartaoId, @RequestBody @Valid BiometriaRequest biometriaRequest, UriComponentsBuilder uriBuilder) {
         Optional<Cartao> cartaoPossivel = cartaoRepository.findById(cartaoId);
         if (cartaoPossivel.isPresent()) {
-            Biometria biometria = biometriaRequest.converteBiometriaRequestParaBiometria(cartaoPossivel.get());
-            biometriaRepository.save(biometria);
-            URI uri = uriBuilder.path("/biometria/{id}").buildAndExpand(biometria.getId()).toUri();
-            return ResponseEntity.created(uri).body(new BiometriaResponse(biometria));
+            if (Base64.isBase64(biometriaRequest.getFingerprint())) {
+                Biometria biometria = biometriaRequest.converteBiometriaRequestParaBiometria(cartaoPossivel.get());
+                biometriaRepository.save(biometria);
+                URI uri = uriBuilder.path("/biometria/{id}").buildAndExpand(biometria.getId()).toUri();
+                return ResponseEntity.created(uri).body(new BiometriaResponse(biometria));
+            } else {
+                return ResponseEntity.badRequest().build();
+            }
         } else {
             return ResponseEntity.notFound().build();
         }
